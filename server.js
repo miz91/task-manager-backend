1b1a024a1091bed4b9a0b917548acd35257e4f9b
Mizanur Rahman <5rahmm79@solent.ac.uk>
2024-05-28 14:36:38 +0100

    Update MongoDB connection string and server configuration

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/taskmanager', {
+const mongoURI = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/taskmanager' ;
+
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // 5 seconds
+  socketTimeoutMS: 45000, // 45 seconds
 })
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));

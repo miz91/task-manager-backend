777fe932e4209fce527f2ab37f7084e86b7d2eec
Mizanur Rahman <5rahmm79@solent.ac.uk>
2024-06-13 23:11:35 +0100

    Increase Mongoose connection timeout

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,53 +1,40 @@
 const Task = require('../models/Task');
 
 exports.createTask = async (req, res) => {
-  const { title, description, completed } = req.body;
   try {
-    const newTask = new Task({ title, description, completed });
+    const newTask = new Task(req.body);
     await newTask.save();
     res.status(201).json(newTask);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ error: 'Unable to create task' });
   }
 };
 
 exports.getTasks = async (req, res) => {
   try {
     const tasks = await Task.find();
-    res.json(tasks);
+    res.status(200).json(tasks);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ error: 'Unable to fetch tasks' });
   }
 };
 
 exports.updateTask = async (req, res) => {
-  const { id } = req.params;
-  const { title, description, completed } = req.body;
   try {
-    const task = await Task.findById(id);
-    if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
-    task.title = title;
-    task.description = description;
-    task.completed = completed;
-    await task.save();
-    res.json(task);
+    const { id } = req.params;
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(updatedTask);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ error: 'Unable to update task' });
   }
 };
 
 exports.deleteTask = async (req, res) => {
-  const { id } = req.params;
   try {
-    const task = await Task.findById(id);
-    if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
-    await task.remove();
-    res.json({ message: 'Task deleted' });
+    const { id } = req.params;
+    await Task.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Task deleted' });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ error: 'Unable to delete task' });
   }
 };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const taskRoutes = require('./routes/taskRoutes'); 
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 app.use(cors());
@@ -20,7 +20,7 @@ mongoose.connect(mongoURI, {
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
 
-app.use('/api', taskRoutes); 
+app.use('/api', taskRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
